1ff63e462190d6a60275012bf9344d5e669d417e
Scott M Cook <SMCRunner88>
2023-04-06 20:28:09 -0700

    Remove boilerplate css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="grid place-content-center text-3xl font-bold underline">
+      <div>Hello world!</div>
+      <div>Hello world!</div>
+    </div>
   );
 }
 
