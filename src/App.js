19775c93b84a9b9c64708bb851eaeeb86cd9655e
Scott M Cook <SMCRunner88>
2023-04-06 20:50:24 -0700

    Move assets into src

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import SkillCard from "./components/SkillCards/SkillCards";
 
 function App() {
   return (
     <div className="grid place-content-center text-3xl font-bold underline">
-      <div>Hello world!</div>
+      <SkillCard />
       <div>Hello world!</div>
     </div>
   );
