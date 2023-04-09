125c8b679a8fd12822fe2b5350d42c591b2f4475
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-09 16:00:43 -0700

    Add content and typography styles

--- src/components/ResultsPanel/ResultsPanel.js
+++ src/components/ResultsPanel/ResultsPanel.js
@@ -1,9 +1,19 @@
 import "./results-panel.css";
+
 export default function ResultsPanel() {
   return (
-    <div className="flex-1 ">
-      <div className="h-52 rounded-b-lg xl:rounded-lg text-center results-panel__background">
-        Your results
+    <div className="flex-1 rounded-b-lg xl:rounded-lg text-center results-panel__background">
+      <div className="flex flex-col">
+        <div className="results-panel__header">Your Result</div>
+        <div className="results-panel__score results-panel__score--background rounded-full">
+          <span className="results-panel__score--bold text-5xl">76</span>
+          <br />
+          of 100
+        </div>
+        <h3>Great</h3>
+        <p>
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
       </div>
     </div>
   );
