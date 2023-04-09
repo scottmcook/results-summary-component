6aef93e728f04fa4c0d846fcdb0732be0a98c11c
Scott M Cook <SMCRunner88>
2023-04-08 21:30:31 -0700

    Add column size and box shadow

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import SkillCard from "./components/SkillCards/SkillCards";
 
 function App() {
   return (
-    <div className="lg:w-1/2 place-content-center">
+    <div className="lg:w-1/2 place-content-center lg:rounded-lg shadow-md">
       <div className="flex flex-col lg:flex-row">
         {/* Result component */}
-        <div className="flex-1">
+        <div className="flex-1 ">
           <div
             style={{
               background:
-                "linear-gradient(to bottom, hsl(234, 85%, 45%), hsl(241, 81%, 54%)",
+                "linear-gradient(0deg, hsl(234, 85%, 45%), hsl(241, 81%, 54%)",
             }}
-            className="h-52 text-center"
+            className="h-52 rounded-b-lg lg:rounded-lg text-center "
           >
             Your results
           </div>
         </div>
-        <div className="flex-1">
+        <div className="flex-1 px-6">
           <h3>Summary</h3>
           <SkillCard />
           <button>Continue</button>
