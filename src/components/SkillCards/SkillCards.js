d978316ec5240528c52065e161ee716a5a590617
Scott M Cook <SMCRunner88>
2023-04-08 18:53:54 -0700

    Add load for images

--- src/components/SkillCards/SkillCards.js
+++ src/components/SkillCards/SkillCards.js
@@ -3,7 +3,7 @@ import scores from '../../data.json';
 export default function SkillCard() {
 	const scoreCard = scores.map(score => 
 		<div className='flex bg-teal-200'>
-			{score.icon}
+			<img src={score.icon} alt={score.category} />
 			{score.category}
 			{score.score} / 100
 		</div>	
