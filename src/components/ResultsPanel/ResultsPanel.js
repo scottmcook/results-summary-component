d856d4fe4b18b324356e8cbb1816297a319c29aa
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-09 15:18:21 -0700

    Refactor to utilize results panel component

---
+++ src/components/ResultsPanel/ResultsPanel.js
@@ -0,0 +1,10 @@
+import "./results-panel.css";
+export default function ResultsPanel() {
+  return (
+    <div className="flex-1 ">
+      <div className="h-52 rounded-b-lg xl:rounded-lg text-center results-panel__background">
+        Your results
+      </div>
+    </div>
+  );
+}
