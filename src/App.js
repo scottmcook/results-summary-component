bf744b2c63b8cb954e3a7b0d68505a43d59d2f77
Scott M Cook <SMCRunner88>
2023-04-08 21:17:14 -0700

    Add basic mobile and desktop layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import SkillCard from "./components/SkillCards/SkillCards";
 
 function App() {
   return (
-    <div className="grid place-content-center text-3xl font-bold underline">
-      {/* Result component */}
-      <div style={{background: 'linear-gradient(to bottom, hsl(234, 85%, 45%), hsl(241, 81%, 54%)'}} className="h-52 text-center">Your results</div>
-      <SkillCard />
-      <button>Continue</button>
+    <div className="lg:w-1/2 place-content-center">
+      <div className="flex flex-col lg:flex-row">
+        {/* Result component */}
+        <div className="flex-1">
+          <div
+            style={{
+              background:
+                "linear-gradient(to bottom, hsl(234, 85%, 45%), hsl(241, 81%, 54%)",
+            }}
+            className="h-52 text-center"
+          >
+            Your results
+          </div>
+        </div>
+        <div className="flex-1">
+          <h3>Summary</h3>
+          <SkillCard />
+          <button>Continue</button>
+        </div>
+      </div>
     </div>
   );
 }
