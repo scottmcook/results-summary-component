47d76db6c18c868a65835be4b74ff9a80450dd68
Scott M Cook <SMCRunner88>
2023-04-08 20:43:58 -0700

    Add basic markup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import SkillCard from "./components/SkillCards/SkillCards";
 function App() {
   return (
     <div className="grid place-content-center text-3xl font-bold underline">
+      {/* Result component */}
+      <div style={{background: 'linear-gradient(to bottom, hsl(234, 85%, 45%), hsl(241, 81%, 54%)'}} className="h-52 text-center">Your results</div>
       <SkillCard />
-      <div>Hello world!</div>
+      <button>Continue</button>
     </div>
   );
 }
