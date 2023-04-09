bdbff9cec8972527ea3b93bbd6e63b64129b22b6
Scott M Cook <SMCRunner88>
2023-04-08 20:44:23 -0700

    Add background color property

--- src/components/SkillCards/SkillCards.js
+++ src/components/SkillCards/SkillCards.js
@@ -2,7 +2,7 @@ import scores from '../../data.json';
 
 export default function SkillCard() {
 	const scoreCard = scores.map(score => 
-		<div className='flex bg-teal-200'>
+		<div className='flex bg-teal-200' style={{backgroundColor: `${score.backgroundColor}`}}>
 			<img src={score.icon} alt={score.category} />
 			{score.category}
 			{score.score} / 100
