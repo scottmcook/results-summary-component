78126bd232b1e2201bb24b22c8b4b134f5e253e6
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-09 21:48:39 -0700

    Add circle styling and fine tune margins

--- src/components/ResultsPanel/ResultsPanel.js
+++ src/components/ResultsPanel/ResultsPanel.js
@@ -1,22 +1,20 @@
-import "./results-panel.css";
+import "./ResultsPanel.css";
 
 export default function ResultsPanel() {
   return (
     <div className="flex-1 rounded-b-3xl xl:rounded-3xl text-center results-panel__background">
-      <div className="flex flex-col">
-        <div className="my-4 results-panel__header">Your Result</div>
-        <div className="my-4 results-panel__score">
-          <div
-            className="results-panel__score--background rounded-full"
-            style={{ backgroundColor: "black" }}
-          >
-            <span className="results-panel__score--bold text-5xl">76</span>
-            <br />
-            of 100
+      <div className="flex flex-col content-center">
+        <div className="mt-6 mb-2 results-panel__header">Your Result</div>
+        <div className="my-4 flex-1 self-center">
+          <div className="results-panel__score flex-col">
+            <div className="mb-2">
+              <span className="results-panel__score--bold text-6xl">76</span>
+            </div>
+            <div className="results-panel__score-p">of 100</div>
           </div>
         </div>
-        <h3 className="my-4">Great</h3>
-        <p className="mt-4 mb-8 px-6 leading-tight">
+        <h3 className="my-2">Great</h3>
+        <p className="mb-10 px-6 leading-tight">
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
