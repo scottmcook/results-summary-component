ca9c127d70fc8a5be806a6f6ae7a1b48d4085811
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-16 16:27:37 -0700

    Add margin and font color

--- src/components/SkillCards/SkillCards.js
+++ src/components/SkillCards/SkillCards.js
@@ -3,14 +3,17 @@ import scores from "../../data.json";
 export default function SkillCard() {
   const scoreCard = scores.map((score) => (
     <div
-      className="flex justify-between p-3 rounded-lg bg-teal-200"
+      className="flex justify-between p-3 mb-6 rounded-lg bg-teal-200"
       style={{ backgroundColor: `${score.backgroundColor}` }}
     >
       <div className="flex flex-initial">
         <img src={score.icon} alt={score.category} />
+        &nbsp;&nbsp;
         <span style={{ color: `${score.fontColor}` }}>{score.category}</span>
       </div>
-      <div className="flex-initial">{score.score} / 100</div>
+      <div className="flex-initial" style={{ color: "var(--dark-gray-blue)" }}>
+        <span className="font-bold">{score.score}</span> / 100
+      </div>
     </div>
   ));
   return <div className="flex flex-col">{scoreCard}</div>;
