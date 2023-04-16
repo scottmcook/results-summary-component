075fde07ecca2ad6e05c2a6cbd1974ff28cb4f2f
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-16 16:12:31 -0700

    Update border, padding, and background

--- src/components/SkillCards/SkillCards.js
+++ src/components/SkillCards/SkillCards.js
@@ -1,16 +1,17 @@
-import scores from '../../data.json';
+import scores from "../../data.json";
 
 export default function SkillCard() {
-	const scoreCard = scores.map(score => 
-		<div className='flex bg-teal-200' style={{backgroundColor: `${score.backgroundColor}`}}>
-			<img src={score.icon} alt={score.category} />
-			{score.category}
-			{score.score} / 100
-		</div>	
-	) 
-	return (
-		<div className='flex flex-col'>
-			{scoreCard}
-		</div>
-	)
+  const scoreCard = scores.map((score) => (
+    <div
+      className="flex justify-between p-3 rounded-lg bg-teal-200"
+      style={{ backgroundColor: `${score.backgroundColor}` }}
+    >
+      <div className="flex flex-initial">
+        <img src={score.icon} alt={score.category} />
+        <span style={{ color: `${score.fontColor}` }}>{score.category}</span>
+      </div>
+      <div className="flex-initial">{score.score} / 100</div>
+    </div>
+  ));
+  return <div className="flex flex-col">{scoreCard}</div>;
 }
