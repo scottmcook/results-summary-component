fa062075ebdd0bafd0577d0078d6778cb5c3ce86
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-22 13:50:06 -0700

    Update app to be center

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import ResultsPanel from "./components/ResultsPanel/ResultsPanel";
 
 function App() {
   return (
-    <div className="xl:w-1/2 place-content-center xl:rounded-3xl shadow-md">
-      <div className="flex flex-col xl:flex-row">
-        <ResultsPanel />
-        <div className="flex-1 px-6">
-          <h3>Summary</h3>
-          <SkillCard />
-          <button className="flex-1 py-2 rounded-full">Continue</button>
+    <div className="flex items-center justify-center h-full">
+      <div className="md:w-1/2 rounded-3xl xl:shadow-md">
+        <div className="flex flex-col xl:flex-row">
+          <ResultsPanel />
+          <div className="flex-1 px-6">
+            <h3>Summary</h3>
+            <SkillCard />
+            <button className="flex-1 py-3 rounded-full">Continue</button>
+          </div>
         </div>
       </div>
     </div>
