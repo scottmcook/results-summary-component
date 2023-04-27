be7988f0ee9d95fdec1572e3559f6aa5e131591a
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-27 11:00:01 -0700

    Update spacing and app position

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import ResultsPanel from "./components/ResultsPanel/ResultsPanel";
 function App() {
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="md:w-1/2 rounded-3xl xl:shadow-md">
+      <div className="rounded-3xl xl:shadow-md">
         <div className="flex flex-col xl:flex-row">
           <ResultsPanel />
           <div className="flex-1 px-6">
             <h3>Summary</h3>
             <SkillCard />
-            <button className="flex-1 py-3 rounded-full">Continue</button>
+            <button className="mb-8 flex-1 py-3 rounded-full">Continue</button>
           </div>
         </div>
       </div>
