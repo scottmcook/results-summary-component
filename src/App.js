1ac7ce07f088ba31810e4387dd7a7621b958a7bd
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-27 11:53:50 -0700

    Update results panel layout

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ function App() {
     <div className="flex items-center justify-center h-full">
       <div className="rounded-3xl xl:shadow-md">
         <div className="flex flex-col xl:flex-row">
-          <ResultsPanel />
-          <div className="flex-1 px-6">
-            <h3>Summary</h3>
+          <div className="xl:w-80 rounded-b-3xl xl:rounded-3xl text-center results-panel__background">
+            <ResultsPanel />
+          </div>
+          <div className="xl:w-80 px-6">
+            <h3 className="text-xl mt-6 my-2">Summary</h3>
             <SkillCard />
-            <button className="mb-8 flex-1 py-3 rounded-full">Continue</button>
+            <button className="my-6 mb-8 flex-1 py-3 rounded-full">
+              Continue
+            </button>
           </div>
         </div>
       </div>
