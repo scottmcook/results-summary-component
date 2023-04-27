da7a5d19f63a8adec86995847386bdbb1ddbd148
Scott M Cook <scott.madison.cook@gmail.com>
2023-04-27 13:10:37 -0700

    Update media query padding

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="flex items-center justify-center h-full">
       <div className="rounded-3xl xl:shadow-md">
         <div className="flex flex-col xl:flex-row">
-          <div className="rounded-b-3xl text-center results-panel__background sm:pb-8 xl:w-80 xl:rounded-3xl">
+          <div className="rounded-b-3xl text-center results-panel__background xl:w-80 xl:rounded-3xl">
             <ResultsPanel />
           </div>
           <div className="xl:w-80 px-6">
